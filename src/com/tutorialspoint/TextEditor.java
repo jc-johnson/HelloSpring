154312691b9b4e3b359963a9b2e9b8b1e2f8168e
Jordan <jjohnson@gorillagroup.com>
2016-05-25 15:08:44 -0500

    Setter-based dependency injection

--- src/com/tutorialspoint/TextEditor.java
+++ src/com/tutorialspoint/TextEditor.java
@@ -6,9 +6,15 @@ package com.tutorialspoint;
 public class TextEditor {
     private SpellChecker spellChecker;
 
-    public TextEditor(SpellChecker spellChecker) {
-        System.out.println("Inside TextEditor constructor.");
+    // A setter method to inject the dependency.
+    public void setSpellChecker(SpellChecker spellChecker) {
+        System.out.println("Inside setSpellChecker.");
         this.spellChecker = spellChecker;
+    }
+
+    // a getter method to return spellChecker
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
     }
 
     public void spellCheck() {
