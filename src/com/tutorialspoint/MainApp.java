2f7e821cfbd6efd78f594cc36c5dff587e2fdc40
Jordan <jjohnson@gorillagroup.com>
2016-05-25 13:04:58 -0500

    Spring Bean Life Cycle

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,6 +1,6 @@
 package com.tutorialspoint;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -9,11 +9,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
     public static void main(String[] args) {
         // loads bean configuration file
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-
         obj.getMessage();
+        context.registerShutdownHook();
     }
 
 }
