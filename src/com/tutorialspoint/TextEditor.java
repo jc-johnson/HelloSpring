9fe7049fd2499c6415836e25693c828d39ea6bbe
Jordan <jjohnson@gorillagroup.com>
2016-05-25 16:52:18 -0500

    Spring autowiring by constructor

--- src/com/tutorialspoint/TextEditor.java
+++ src/com/tutorialspoint/TextEditor.java
@@ -7,19 +7,14 @@ public class TextEditor {
     private SpellChecker spellChecker;
     private String name;
 
-    // A setter method to inject the dependency.
-    public void setSpellChecker(SpellChecker spellChecker) {
-        System.out.println("Inside setSpellChecker.");
+    public TextEditor(SpellChecker spellChecker, String name) {
         this.spellChecker = spellChecker;
+        this.name = name;
     }
 
     // a getter method to return spellChecker
     public SpellChecker getSpellChecker() {
         return spellChecker;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getName() {
