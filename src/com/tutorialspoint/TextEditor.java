2bfeb3e8965ec90872100b943f3257751a81a0f8
Jordan <jjohnson@gorillagroup.com>
2016-05-26 11:27:35 -0500

    @autowired on constructors

--- src/com/tutorialspoint/TextEditor.java
+++ src/com/tutorialspoint/TextEditor.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Created by jjohnson on 5/25/2016.
  */
 public class TextEditor {
-    @Autowired
+
     private SpellChecker spellChecker;
 
-    public TextEditor(){
+    @Autowired
+    public TextEditor(SpellChecker spellChecker){
         System.out.println("Inside TextEditor constructor.");
+        this.spellChecker = spellChecker;
     }
 
     public SpellChecker getSpellChecker() {
