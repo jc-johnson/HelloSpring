d889122e3cce9af452d530bd891f74f55715b4ff
Jordan <jjohnson@gorillagroup.com>
2016-05-26 11:29:55 -0500

    @autowired with (required=false) option

--- src/com/tutorialspoint/Student.java
+++ src/com/tutorialspoint/Student.java
@@ -1,6 +1,6 @@
 package com.tutorialspoint;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by jjohnson on 5/26/2016.
@@ -9,7 +9,7 @@ public class Student {
     private Integer age;
     private String name;
 
-    @Required
+    @Autowired(required=false)
     public void setAge(Integer age) {
         this.age = age;
     }
@@ -18,7 +18,7 @@ public class Student {
         return age;
     }
 
-    @Required
+    @Autowired
     public void setName(String name){
         this.name = name;
     }
