3ddba9aa215690a84bf443ccca3574e32b58af02
Jordan <jjohnson@gorillagroup.com>
2016-05-27 10:40:25 -0500

    @AspectJ based AOP with Spring

---
+++ src/com/tutorialspoint/AspectModule.java
@@ -0,0 +1,12 @@
+package com.tutorialspoint;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * Created by jjohnson on 5/27/2016.
+ */
+
+@Aspect
+public class AspectModule {
+
+}
--- src/com/tutorialspoint/Logging.java
+++ src/com/tutorialspoint/Logging.java
@@ -1,14 +1,26 @@
 package com.tutorialspoint;
 
+import org.aspectj.lang.annotation.*;
+
 /**
  * Created by jjohnson on 5/26/2016.
  */
+@Aspect
 public class Logging {
+
+    /**
+     * Following is the definition for a pointcut to select
+     * all the methods available. So advice will be called
+     * for all the methods
+     */
+    @Pointcut("execution(* com.tutorialspoint.*.*(..))")
+    private void selectAll(){}
 
     /**
      * This is the method which I would like to execute
      * before a selected method execution.
      */
+    @Before("selectAll()")
     public void beforeAdvice(){
         System.out.println("Going to setup student profile");
     }
@@ -17,6 +29,7 @@ public class Logging {
      * This is the method which I would like to execute
      * after a selected method execution.
      */
+    @After("selectAll()")
     public void afterAdvice(){
         System.out.println("Student profile has been setup.");
     }
@@ -25,6 +38,7 @@ public class Logging {
      * This is the method which I would like to execute
      * when any method returns.
      */
+    @AfterReturning(pointcut = "selectAll()", returning="retVal")
     public void afterReturningAdvice(Object retVal){
         System.out.println("Returning:" + retVal.toString());
     }
@@ -33,6 +47,7 @@ public class Logging {
      * This is the method which I would like to execute
      * if there is an exception raised.
      */
+    @AfterThrowing(pointcut = "selectAll()", throwing = "ex")
     public void AfterThrowingAdvice(IllegalArgumentException ex){
         System.out.println("There has been an exception: " + ex.toString());
     }
